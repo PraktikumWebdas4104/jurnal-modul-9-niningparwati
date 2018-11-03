a60c394e6751c0ccd6b4bff32a4b1ae7dcd20271
niningparwati <42663455+niningparwati@users.noreply.github.com>
2018-11-03 11:35:52 +0700

    Add files via upload

---
+++ database.php
@@ -0,0 +1,59 @@
+<?php 
+
+class database{
+
+	var $host = "localhost";
+	var $uname = "root";
+	var $pass = "";
+	var $db = "tabel"; //isi sesuai nama database anda
+
+	function __construct(){
+		$this->conn = mysqli_connect($this->host,$this->uname,$this->pass,$this->db);
+		//buatlah koneksi secara OOP
+		
+	}
+
+	function tampil_data(){
+		//lengkapilah method tampil data
+		//query select user
+		$data = mysqli_query($this->conn, "SELECT * FROM user");
+		while($d = mysqli_fetch_array($data)){
+			$hasil[] = $d;
+		}
+		return $hasil;
+
+	}
+
+	function input($nama,$alamat,$usia){
+		//buatlah method input
+		//query inset into user
+		mysqli_query($this->conn, "INSERT INTO user VALUES('','$nama','$alamat','$usia')");
+
+	}
+
+	function hapus($id){
+		mysqli_query($this->conn, "DELETE FROM user WHERE id='$id'");
+		//buatlah method hapus
+		//query delete from id where id ='$id'
+	}
+
+	function edit($id){
+		//lengkapilah method edit
+		//query select from user where id ='$id'
+		$data = mysqli_query($this->conn, "SELECT * FROM user WHERE id='$id'");
+
+		while($d = mysqli_fetch_array($data)){
+			$hasil[] = $d;
+		}
+		return $hasil;
+	}
+
+	function update($id,$nama,$alamat,$usia){
+		mysqli_query($this->conn, "UPDATE user SET nama='$nama', alamat='$alamat', usia='$usia' WHERE id='$id'");
+		//buatlah method update
+		//query update user set blablabla where id='$id'
+	}
+
+} 
+
+?>
